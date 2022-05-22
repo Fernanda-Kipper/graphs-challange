40303a326f2ca7da623c4ccad72d9873e15dad07
Fernanda Kipper <nanda.kipper@gmail.com>
2022-05-22 14:14:19 -0300

    feat: update calculateDistance to TS file

--- src/utils/calculateDistance.ts
+++ src/utils/calculateDistance.ts
@@ -1,5 +1,5 @@
 import { DISTANCES, VERTICES } from '../constants/distances'
 
-export const calculateDistance = (origin, destination) => {
+export const calculateDistance = (origin: string, destination: string) => {
   // TODO: faça seu algortimo aqui, faça a função da forma que achar melhor, inclusive os parâmetros
 }
